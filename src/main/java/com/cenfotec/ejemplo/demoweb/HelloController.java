c633c7ac1b08ca0d2c9729d96971064f6742988c
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2019-10-07 20:38:08 -0600

    first commit

---
+++ src/main/java/com/cenfotec/ejemplo/demoweb/HelloController.java
@@ -0,0 +1,17 @@
+package com.cenfotec.ejemplo.demoweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/geeting")
+    public String eliminarListar(@RequestParam(name="name", required=false, defaultValue="world")String name, Model model) {
+        model.addAttribute("name", name);
+        return "greetings";
+    }
+
+}
